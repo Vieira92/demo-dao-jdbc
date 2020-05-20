d9d881ea4d24379a36a0f85ec2f50b7655b0014e
Bruno Vieira <vieiraonwork@gmail.com>
2020-05-20 01:30:10 +0100

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -17,6 +19,8 @@ public class Program {
 		System.out.println();
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 		System.out.println(seller);
+		
+		
 	}
 
 }
